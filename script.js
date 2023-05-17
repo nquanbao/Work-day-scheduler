2acdbde2e245feea3e29f1ae6584778447110535
Bao Nguyen <bnguyen88@knights.ucf.edu>
2023-05-17 16:01:17 -0400

    adding comment

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
 
 var currentTime = $('#currentDay')
 var timecheck = $('.time-block');
@@ -66,32 +43,18 @@ function localSaveEl (blocktimeEvent, valueDescripton) {
     saveLocal.push(inputValue)
     localStorage.setItem(blocktimeEvent, JSON.stringify(saveLocal))
 }
-
+//Loop to get value for each block-time from local storage 
 for( var i = 0; i < timecheck.length; i++){
   var idTake = timecheck[i].getAttribute('id')
   var aaa = localStorage.getItem(idTake)
-  // console.log(aaa)
   timecheck[i].children[1].innerHTML = JSON.parse(localStorage.getItem(idTake))
-  // var valShow = idTake.children[1]
-  // var aaaa = valShow.value
-  // console.log(aaaa)
-  // valShow.innerHTML = localStorage.getItem(idTake)
 }
 
-
+//AddEventlistner for the button when clicked, creating the Key for local storage by id value
+//in containing block-time and input the value
 btnSaveEl.on("click", function(e){
-  
-  // var saveLocal = JSON.parse(localStorage.getItem("blockEvent")) || []
   var idClick = this.parentNode;
-  // console.log(idClick)
   var idGetValue = idClick.getAttribute('id')
   var hhhhh = idClick.children[1].value
-  // console.log(idGetValue)
-  // console.log(hhhhh)
-  // // console.log(idGetValue)
-  // // console.log(kkk)
-  // localSaveEl(idGetValue,hhhhh)
-  // saveLocal.push(hhhhh)
-  // localStorage.setItem(idGetValue, JSON.stringify(saveLocal))
   localSaveEl(idGetValue,hhhhh)
 })
